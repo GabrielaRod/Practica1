e9548ce8b835f2a8538edebfc73e2ff3d4c4d7da
Felix A. Guzman <felixalejandro@outlook.com>
2018-05-19 00:20:30 -0400

    Creando cantidad de parrafos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,11 @@
+import org.jsoup.Connection;
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -9,6 +17,48 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         String entrda = scanner.nextLine();
-        System.out.println("Entrada: " + entrda);
+        //System.out.println("Entrada: " + entrda);
+
+        if (validarURL(entrda)) {
+            System.out.println("URL valida!");
+            System.out.println("Cantidad de Parrafos: " + cantidadParrafos(entrda));
+
+        }
+        else {
+            System.out.println("URL invalida!");
+        }
+    }
+
+    public static boolean validarURL(String url){
+
+        try {
+            Connection.Response response = Jsoup.connect(url).execute();
+            if (response.statusCode() == 200)
+            {
+                return true;
+            }
+        } catch (HttpStatusException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public static int cantidadParrafos(String url){
+
+        int cantidad = 0;
+
+        Document document = Jsoup.parse(url);
+       Elements elements = document.select("p");
+
+        for (Element element: elements) {
+
+
+            cantidad++;
+        }
+
+        return cantidad;
     }
 }
