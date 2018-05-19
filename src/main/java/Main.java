91fcac0be7da4c1863dace43581c61f4b078c726
Felix A. Guzman <felixalejandro@outlook.com>
2018-05-18 23:56:12 -0400

    Creando proyecto

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Digite una URL: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String entrda = scanner.nextLine();
+        System.out.println("Entrada: " + entrda);
+    }
+}
