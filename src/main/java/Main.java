41bc0fd05da1264aee221e931f58ae03fd063dc5
Felix A. Guzman <felixalejandro@outlook.com>
2018-05-19 15:58:04 -0400

    Creando cantidad de formularios y mostrando input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,25 +3,32 @@ import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
 import org.jsoup.select.Elements;
 
+import javax.print.Doc;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
+    private static Document document;
     public static void main(String[] args) {
 
         System.out.println("Digite una URL: ");
 
-        Scanner scanner = new Scanner(System.in);
+       // Scanner scanner = new Scanner(System.in);
 
-        String entrda = scanner.nextLine();
+        String entrda = "https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Your_first_HTML_form";//scanner.nextLine();
         //System.out.println("Entrada: " + entrda);
 
         if (validarURL(entrda)) {
             System.out.println("URL valida!");
+
             System.out.println("Cantidad de Parrafos: " + cantidadParrafos(entrda));
+            System.out.println("Cantidad de Fotos dentro de parrafos: " + cantidadFotos(entrda));
+
+            cantidadForm(entrda);
 
         }
         else {
@@ -29,36 +36,69 @@ public class Main {
         }
     }
 
-    public static boolean validarURL(String url){
+    private static boolean validarURL(String url){
 
         try {
-            Connection.Response response = Jsoup.connect(url).execute();
-            if (response.statusCode() == 200)
-            {
-                return true;
-            }
-        } catch (HttpStatusException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        return false;
+            document = Jsoup.connect(url).get();
+
+
+        } catch (IOException e) {
+            //e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
-    public static int cantidadParrafos(String url){
+        private static int cantidadParrafos(String url) {
 
         int cantidad = 0;
 
-        Document document = Jsoup.parse(url);
-       Elements elements = document.select("p");
-
-        for (Element element: elements) {
-
-
-            cantidad++;
-        }
+        Elements elements = document.select("p");
+        cantidad = elements.size();
 
         return cantidad;
+    }
+
+    private static int cantidadFotos(String url){
+
+        int cantidad =0;
+
+        Elements elements = document.select("p img");
+        cantidad = elements.size();
+
+        return cantidad;
+    }
+
+    private static void cantidadForm(String url){
+
+        int  cantidadPost =0, cantidadGet =0, form=1;
+
+        Elements formElement = document.select("[method=post]");
+        cantidadPost = formElement.size();
+        System.out.println("Cantidad de formularios con el metodo POST: " + cantidadPost);
+
+        formElement = document.select("[method=get]");
+        cantidadGet = formElement.size();
+        System.out.println("Cantidad de formularios con el metodo GET: " + cantidadGet);
+
+        Elements elements = document.select("form");
+
+        for (Element element:elements) {
+
+            System.out.println("Formulario: #"+form);
+
+            Elements inputs = element.select("input");
+            for (Element element1: inputs) {
+                System.out.println("Tipo: " + element1.attr("type"));
+            }
+
+            System.out.println("");
+            form++;
+            
+
+        }
+
+
     }
 }
